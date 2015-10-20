8dfa0c60b53ef7bd062925f5b7f54c49c9a1d844
Luzian Zagadinow <luzianz@outlook.com>
2015-10-20 08:04:37 -0400

    add class for generic credentials

---
+++ LZ.Security.OAuth/Portable/ConsumerCredentials.cs
@@ -0,0 +1,21 @@
+﻿namespace LZ.Security.OAuth
+{
+	public struct ConsumerCredentials : ICredential
+	{
+		public ConsumerCredentials(string key, string secret)
+		{
+			this.Key = key;
+			this.Secret = secret;
+		}
+
+		public string Key
+		{
+			get; set;
+		}
+
+		public string Secret
+		{
+			get; set;
+		}
+	}
+}
