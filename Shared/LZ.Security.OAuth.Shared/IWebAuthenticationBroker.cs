f1f462c4eb391939f76971ff1ca8310303e932a9
Luzian Zagadinow <luzianz@outlook.com>
2018-02-09 16:14:07 -0500

    added IWebAuthenticationBroker.cs

---
+++ Shared/LZ.Security.OAuth.Shared/IWebAuthenticationBroker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace LZ.Security.OAuth {
+
+	public interface IWebAuthenticationBroker {
+
+		Task<ICredential> AuthenticateAsync(Uri requestUri, Uri callbackUri = null);
+		Uri GetCurrentApplicationCallbackUri();
+	}
+}
